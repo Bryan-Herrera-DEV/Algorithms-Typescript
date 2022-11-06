cc24ffbb8da17826bc74a21a1ef1d254d04bc0fc
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2022-11-06 17:58:32 -0500

    [:white_check_mark: add CI] añade Continous Integration al repositorio

--- src/math/Tests/GnomeSort.test.ts
+++ src/math/Tests/GnomeSort.test.ts
@@ -1,19 +1,19 @@
-import { gnomeSort } from "../GnomeSort";
+import { gnomeSort } from '../GnomeSort'
 
 describe("Probando algoritmo 'Gnome Sort'", () => {
   const testsCases: number[][] = [
     [],
-    [2,1],
-    [3,2,1],
-    [1,1,1,1,1,1,1,1,1,1,1],
-    [8,5,6,9,7,2,1,1,8,10,6]
-  ];
+    [2, 1],
+    [3, 2, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [8, 5, 6, 9, 7, 2, 1, 1, 8, 10, 6]
+  ]
   test.each(testsCases)(
     'Deeberia devolver el valor correcto del caso de prueba: %#',
     (...arr: number[]) => {
       expect(gnomeSort([...arr])).toStrictEqual(
         [...arr].sort((a: number, b: number) => a - b)
-      );
+      )
     }
-  );
-});
+  )
+})
