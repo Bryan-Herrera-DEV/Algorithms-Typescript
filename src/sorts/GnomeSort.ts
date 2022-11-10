741927e857950132799edcf401b048371c3a9720
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2022-11-10 11:54:29 -0500

    [:hammer: refactor] cambia los estilos de los archivos para ajustarlos al linter

--- src/sorts/GnomeSort.ts
+++ src/sorts/GnomeSort.ts
@@ -5,18 +5,18 @@
  * @return: (number[]) Array ya ordenado
  */
 
- export const gnomeSort = (arr: number[]): number[] =>{
-  if(arr.length <= 1) {
+export const gnomeSort = (arr: number[]): number[] => {
+  if (arr.length <= 1) {
     return arr;
   }
   let i: number = 1;
-  while(i < arr.length){
-    if(arr[i-1] <= arr[i]) {
+  while (i < arr.length) {
+    if (arr[i - 1] <= arr[i]) {
       i++; // Incrementamos el indice de laposicion si la submatriz [i-1;i] ya esta ordenado
     } else {
-      [arr[i], arr[i-1]] = [arr[i-1], arr[i]];
+      [arr[i], arr[i - 1]] = [arr[i - 1], arr[i]];
       i = Math.max(1, i - 1); // devolvernos al indice anterior para comprobar el intercambio
     }
   }
   return arr;
-}
+};
