e2e1b44458da03043488c4d80c41de125f9aad8b
Bryan-Herrera-DEV <hbryanelias@gmail.com>
2022-11-19 15:38:38 -0500

    [✨ feat](#3)añado los testing para la funcion BINARY SEARCH

---
+++ src/algorithms/search/BinarySearch/BinarySearch.ts
@@ -0,0 +1,15 @@
+/**
+ * @function BinarySearch
+ * @description algoritmo de búsqueda binaria (implementaciones iterativas y recursivas) para una matriz ordenada.
+ * El algoritmo busca un valor específico en una matriz ordenada en tiempo logarítmico.
+ * Repetidamente divide por la mitad la porción de la lista que podría contener el ítem,
+ *  hasta que ha reducido los posibles índices a sólo uno.
+ *
+ * @param: {number[]} array - Lista ordenada de numeros
+ * @param: {number} target - Numero a buscar
+ * @return: {number} - índice del número de destino en la lista, o -1 si no se encuentra
+ */
+
+export const binarySearchIterative = (array: number[], target: number): number => {
+  return 0;
+};
