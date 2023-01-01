7caef311f8a51edad6fefed51321f78c4b7cb87b
Bryan Herrera ~ ርᚱ1ናተᛰ ᚻህᚥተპᚱ <50712646+Bryan-Herrera-DEV@users.noreply.github.com>
2023-01-01 12:29:29 -0500

    [:white_check_mark: add test] agrega test para quicksort

---
+++ src/algorithms/sorts/QuickSort/QuickSort.ts
@@ -0,0 +1,8 @@
+/**
+ * @function QuickSort
+ * @description: El algoritmo Quicksort es un algoritmo de ordenamiento muy utilizado y eficiente. Funciona dividiendo el arreglo en dos subarreglos y colocando el elemento pivot en su posición correcta en el arreglo ordenado. Luego, se llama recursivamente a sí mismo para ordenar cada uno de los subarreglos.
+ * @param: array {number[]} - Arreglo de numeros a ordenar
+ * @param: left {number} - Indice izquierdo del arreglo
+ * @param: right {number} - Indice derecho del arreglo
+ * @return: {number[]} - Arreglo ordenado
+ */
